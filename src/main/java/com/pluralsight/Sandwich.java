945fe20a12fb02952d83b73d03bc4635cd28222d
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-05-22 17:06:16 -0400

    Override Sandwich Method, Create (toastTheSandwich,addToppings, addSauce, addSide) methods.

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -28,15 +28,73 @@ public class Sandwich implements Product{
         this.sauces = new HashSet<>();
         this.sides = new HashSet<>();
     }
-    //TODO : Create the method
+    public void toastTheSandwich(){this.toasted = true;}
+    public void addToppings(Topping topping){this.toppings.add(topping);}
+    public void addSauce(Sauce sauce){this.sauces.add(sauce);}
+    public void addSide(Side side){this.sides.add(side);}
+
+
+    //Done: Create the method
     @Override
     public double getPrice() {
-        return 0;
+        double totalPrice = 0;
+        switch (this.size){
+            case 4->{
+                totalPrice += 7.25;
+                if (extraMeat)
+                    totalPrice += 0.50;
+                if (extraChees)
+                    totalPrice += 0.30;
+                return totalPrice;
+            }
+            case 8->{
+                totalPrice += 10.5;
+                if (extraMeat)
+                    totalPrice += 1;
+                if (extraChees)
+                    totalPrice += 0.60;
+                return totalPrice;
+            }
+            case 12->{
+                totalPrice += 13.75;
+                if (extraMeat)
+                    totalPrice += 1.50;
+                if (extraChees)
+                    totalPrice += 0.90;
+                return totalPrice;
+            }
+            default ->
+                    throw new RuntimeException();
+        }
     }
-    //TODO : Create the method
+    // Done: Create the method
     @Override
     public String getProductName() {
-        return "";
+        StringBuilder productName = new StringBuilder();
+        productName.append("Sandwich Size: ").append(this.size).append(" Bread: ").append(this.bread).append(" Meat: ").append(this.meat).append(" Cheese: ").append(this.cheese).append("\n");
+        if (extraMeat)
+            productName.append("Extra meat\n");
+        if (extraChees)
+            productName.append("Extra cheese\n");
+        if(!toppings.isEmpty()) {
+            productName.append("Toppings:\n");
+            for (Topping topping : toppings)
+                productName.append(topping).append("\t");
+        }
+        if (!sauces.isEmpty()) {
+            productName.append("\nSauces:\n");
+            for (Sauce sauce : sauces)
+                productName.append(sauce).append("\t");
+        }
+        if (!sides.isEmpty()){
+            productName.append("\nSides:\n");
+            for (Side side : sides)
+                productName.append(side).append("\t");
+        }
+        if (toasted)
+            productName.append("\nToasted\n");
+        productName.append("Total----------------------------------").append(getPrice());
+
+        return productName.toString();
     }
 }
-
