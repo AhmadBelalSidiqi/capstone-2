c21051834720bd33c625218f34d50efc5d8abb2d
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-05-25 16:14:07 -0400

    Order Class Created

---
+++ src/main/java/com/pluralsight/Drink.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+import com.pluralsight.enums.Size;
+
+public class Drink implements Product{
+    private final Size size;
+
+    public Drink(Size size) {
+        this.size = size;
+    }
+
+    @Override
+    public double getPrice() {
+        switch (size){
+            case LARGE -> {
+                return 3;
+            }
+            case MEDIUM -> {
+                return 2.50;
+            }
+            case SMALL ->{
+                return 2;
+            }
+            default ->
+                throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public String getProductName() {
+        return "Drink\nSize: "+this.size+"\nPrice_____________________________"+getPrice();
+    }
+}
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,11 +4,21 @@ import com.pluralsight.enums.*;
 
 public class Main {
     public static void main(String[] args) {
-        Sandwich s = new Sandwich(12,Bread.WHEAT, Meat.CHICKEN,true,Cheese.AMERICAN,true);
+        Sandwich s = new Sandwich(12,Bread.WHEAT);
+        s.addMeat(Meat.CHICKEN);
+        s.addSide(Side.SAUCE);
+        s.addSide(Side.AU_JUS);
+        s.addExtraCheese();
+        s.addExtraMeat();
+        s.addCheese(Cheese.AMERICAN);
+        s.addToppings(Topping.CUCUMBERS);
+        s.addToppings(Topping.LETTUCE);
         s.toastTheSandwich();
         Chip c = new Chip();
+        Drink d = new Drink(Size.LARGE);
         System.out.println(c.getProductName());
         System.out.println(s.getProductName());
+        System.out.println(d.getProductName());
 
 
     }
